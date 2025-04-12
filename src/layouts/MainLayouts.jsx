628b6d4cd4cb77430e5e041e90e172aa7436893b
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2025-04-12 13:52:16 +0600

    coffee house project is started here

---
+++ src/layouts/MainLayouts.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+const MainLayouts = () => {
+    return (
+        <div>
+            {/* navbar  */}
+            <Navbar></Navbar>
+            {/* dynamich section  */}
+            {/* footer  */}
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default MainLayouts;
