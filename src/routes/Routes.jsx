0da835a9a79adf5976d67eb5559299932d595521
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2025-04-12 14:03:07 +0600

    navbar section and footer section is imported from daisy ui

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,10 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
+import MainLayouts from "../layouts/MainLayouts";
 
 const routes = createBrowserRouter([
   {
     path: "/",
-    element: <App></App>,
+    element: <MainLayouts></MainLayouts>,
   },
 ]);
 
