bf62e1be8dfbfca8684b02ea93633cfc523ef4b8
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2025-04-13 16:34:21 +0600

    rating section is done here

--- src/pages/Coffees.jsx
+++ src/pages/Coffees.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import Card from "../components/Card";
 
 const Coffees = () => {
-  const data = useLoaderData();
+    const data = useLoaderData();
+    const [coffees,setCoffees]=useState(data)
+
+    const handleSort = (sortBy)=>{
+        if(sortBy==='popularity'){
+            const sorted = [...data].sort((a ,b)=>b.popularity-a.popularity);
+            setCoffees(sorted)
+        }
+        else if(sortBy==='rating'){
+            const sorted = [...data].sort((a ,b)=>b.rating-a.rating);
+            setCoffees(sorted)
+        }
+    }
   return (
     <>
       <div className="flex w-[80%] mx-auto justify-between my-12 items-center">
@@ -13,13 +25,13 @@ const Coffees = () => {
           </h1>
         </div>
         <div className="flex gap-4 ">
-          <button className="btn btn-warning text-white">sort by popularity</button>
-          <button className="btn btn-warning text-white">sort by rating</button>
+          <button onClick={()=>handleSort('popularity')} className="btn btn-primary text-white">sort by popularity</button>
+          <button onClick={()=>handleSort('rating')} className="btn btn-primary text-white">sort by rating</button>
         </div>
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-[80%] mx-auto mt-5">
-        {data.map((coffee, idx) => (
+        {coffees.map((coffee, idx) => (
           <Card key={idx} coffee={coffee} />
         ))}
       </div>
